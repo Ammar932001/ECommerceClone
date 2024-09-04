f15729a4fcce4c0efa65d666f602fabe5d723db0
Ammar_FEnd-08 <ammar.shafqat@ki5.co.uk>
2024-09-04 15:18:54 +0500

    initial commit

--- pages/category/[...id].vue
+++ pages/category/[...id].vue
@@ -1,22 +1,24 @@
 <template>
-    <div v-for="item in cat" :key="cat">
-        {{ item.id }}
-        {{ item.url }}
-        {{ item.title }}
-        
-        
+    <div class="container mx-auto">
+        <div class="flex gap-6">
+        <div class="mt-4 max-w-[270px] max-h-[350px]" v-for="item in cat" :key="cat">
+        <nuxt-link to="">
+         <image-box 
+           :image-url="item.url"
+           :title="item.title"
+           :original-price="item.original_price"
+           :sale-price="item.final_price"
+           :rating-stars="item.stars"
+           :total-rating="item.reviews"       
+           />
+        </nuxt-link> 
+        </div>        
+    </div>
     </div>
 </template>
-
 <script setup>
 import { FetchCategory } from '~/composables/Category/FetchCategory';
-
 const route = useRoute()
 let id = parseInt(route.params.id)
 const cat = await FetchCategory(id)
-console.log(">>>>>cat", cat)
 </script>
-
-<style lang="scss" scoped>
-
-</style>
